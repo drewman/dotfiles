76d272e8cefae727f493562ca6d763993d7bba72
Drew Bruce <drew.bruce@nike.com>
2019-09-16 10:50:46 -0700

    added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude "bootstrap.sh" \
+        --exclude "README.md" \
+        -avh --no-perms . ~;
+    source ~/.bash_profile;
