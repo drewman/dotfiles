a8c91bdf52d5f934084e96ef2abb77ad24e1c569
Drew Bruce <drew.bruce@nike.com>
2019-09-16 11:33:43 -0700

    added homebrew bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,11 @@ rsync --exclude ".git/" \
         --exclude ".DS_Store" \
         --exclude "bootstrap.sh" \
         --exclude "README.md" \
-        -avh --no-perms . ~;
-    source ~/.bash_profile;
+        --exclude "brew.sh" \
+        -avh --no-perms . ~
+
+if [ $(uname) -eq "Darwin" ]
+    ./brew.sh
+fi
+
+source ~/.bash_profile
---
+++ brew.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z $(which brew) ]
+    echo "Installing homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "Installing packages I always want"
+brew update
+brew install wget
+brew install vim
+brew install bash-completion
+brew install pyenv
+brew install pipenv
+brew install virtualenvwrapper
+
+mkdir $HOME/etc
+if [ ! -f $HOME/etc/git-prompt.sh ]
+    echo "wget git-prompt"
+    wget -P $HOME/etc/ https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+fi
+if [ ! -f $HOME/etc/git-completion.bash ]
+    echo "wget git-completion"
+    wget -P $HOME/etc/ https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+fi
