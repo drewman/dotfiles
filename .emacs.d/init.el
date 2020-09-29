a07cc1520f556db6009dce25567c52a66da0f08b
Drew Bruce <drew.bruce@nike.com>
2020-09-29 16:48:20 -0700

    organize emacs conf

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,85 +42,21 @@
 
 (setq initial-scratch-message ";;C-j evaluate\n;;C-x C-f to save buffer\n\n")
 
-;; defaults:
+;; clean up UI
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+(set-fringe-mode 10)
+(show-paren-mode 1)
+
 ;; turn off bell
 (setq ring-bell-function 'ignore)
-(defalias 'yes-or-no-p 'y-or-n-p)
-;; https://www.emacswiki.org/emacs/SavePlace
-(save-place-mode 1)
-(show-paren-mode 1)
-;(setq-default indicate-empty-lines t)
-;(setq cursor-in-non-selected-windows 'hollow)
-;(setq highlight-nonselected-windows t)
-;(setq select-enable-clipboard t)
 
-;; -----------
-;; Indentation
-;; ===========
-
-;; yes, both are needed!
-(setq default-tab-width 4)
-(setq tab-width 4)
-(setq default-fill-column 80)
-(setq fill-column 80)
-(setq-default evil-indent-convert-tabs nil)
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq-default evil-shift-round nil)
-
-;; allows turning off of minor modes in modeline
-(use-package diminish)
-
-; spelling
-
-(use-package spell-fu
-  :diminish
-  :init
-  (setq spell-fu-faces-exclude '(org-meta-line org-link org-code))
-  :config
-  (global-spell-fu-mode))
-
-
-;; add better welcome screen
-;; dashboard prereqs
-(use-package page-break-lines)
-(use-package all-the-icons)
-(use-package dashboard
-  :requires (page-break-lines all-the-icons)
-  :init
-  (setq dashboard-startup-banner 'logo)
-  :config
-  ;; force dashboard to add hooks when command line args are present (making compatible with chemacs)
-  (add-hook 'after-init-hook (lambda () (dashboard-insert-startupify-lists)))
-  (add-hook 'emacs-startup-hook '(lambda ()
-                                   (switch-to-buffer "*dashboard*")
-                                   (goto-char (point-min))
-                                   (redisplay))))
-
-;; better no splash if dashboard it doesn't work
-(setq inhibit-startup-screen t)
-(setq initial-buffer-choice "~/.custom-emacs.d/init.el")
-
-;; this doesn't work right with magit :((
-;(use-package golden-ratio
-;  :diminish
-;  :init
-;  (setq golden-ratio-exclude-modes 'magit-mode)
-;  :config
-;  (golden-ratio-mode 1))
-
-;(use-package doom-modeline
-;  :hook (after-init . doom-modeline-mode)
-;  :init
-;  (setq doom-modeline-project-detection 'project)
-;  (setq doom-modeline-vcs-max-length 40)
-;  (setq doom-modeline-indent-info t))
-
+(set-face-attribute 'default nil :family "JetBrains Mono" :height 140)
 (use-package modus-vivendi-theme
   :init
-  (set-face-attribute 'default nil :family "JetBrains Mono" :height 140)
-  (setq modus-vivendi-theme-bold-constructs t)
-  (setq modus-vivendi-theme-slanted-constructs t)
+  (setq modus-vivendi-theme-bold-constructs t) (setq modus-vivendi-theme-slanted-constructs t)
   (setq modus-vivendi-theme-mode-line 'moody)
   (setq modus-vivendi-theme-completions 'opinionated)
   (setq modus-vivendi-theme-fringes 'intense)
@@ -137,6 +73,7 @@
   :config
   (load-theme 'modus-vivendi t))
 
+;; MODELINE
 ; likely want doom-modeline but moody is a minimalistic skin for modeline
 (use-package moody
   :config
@@ -144,9 +81,73 @@
   (moody-replace-mode-line-buffer-identification)
   (moody-replace-vc-mode))
 
+;(use-package doom-modeline
+;  :hook (after-init . doom-modeline-mode)
+;  :init
+;  (setq doom-modeline-project-detection 'project)
+;  (setq doom-modeline-vcs-max-length 40)
+;  (setq doom-modeline-indent-info t))
+
+;; allows turning off of minor modes in modeline
+(use-package diminish)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+;; STARTPAGE
+;; add better welcome screen
+;; dashboard prereqs
+(use-package page-break-lines)
+(use-package all-the-icons)
+(use-package dashboard
+  :requires (page-break-lines all-the-icons)
+  :init
+  (setq dashboard-startup-banner 'logo)
+  :config
+  ;; force dashboard to add hooks when command line args are present (making compatible with chemacs)
+  (add-hook 'after-init-hook (lambda () (dashboard-insert-startupify-lists)))
+  (add-hook 'emacs-startup-hook '(lambda ()
+                                   (switch-to-buffer "*dashboard*")
+                                   (goto-char (point-min))
+                                   (redisplay))))
+;; WOULD BE BETTER TO JUST OPEN A CUSTOM ORG FILE
+; (setq inhibit-startup-screen t)
+; (setq initial-buffer-choice "~/.custom-emacs.d/init.el")
+
+;; -----------
+;; Indentation
+;; ===========
+
+;; yes, both are needed!
+(setq default-tab-width 4)
+(setq tab-width 4)
+(setq default-fill-column 80)
+(setq fill-column 80)
+(setq-default evil-indent-convert-tabs nil)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq-default evil-shift-round nil)
+
+; spelling-check
+
+;; spell-fu is lightning fast
+(use-package spell-fu
+  :diminish
+  :init
+  (setq spell-fu-faces-exclude '(org-meta-line org-link org-code))
+  :config
+  (global-spell-fu-mode))
+
+;; this doesn't work right with magit :((
+;(use-package golden-ratio
+;  :diminish
+;  :init
+;  (setq golden-ratio-exclude-modes 'magit-mode)
+;  :config
+;  (golden-ratio-mode 1))
+
 ;; Completion
 ;; Would be interesting to try the minimalist approach of using icomplete
 ;; https://protesilaos.com/dotemacs/#h:b6b13f4f-91f5-416d-9934-b51b4d87770c
+;; Strangely, ivy is less configuration
 (use-package swiper
   :init
   (setq swiper-goto-start-of-match t))
@@ -223,7 +224,6 @@
 
 (use-package evil-magit
   :requires (magit evil))
-
 
 (provide 'init.el)
 ;;; init.el ends here
