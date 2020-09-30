fbc2d15d8736495c124d2a06f7f0be449df4d968
drewman <drew@drewbruce.com>
2020-09-29 21:48:18 -0700

    force file save before magit commands

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -200,6 +200,7 @@
 (use-package magit
   :init
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
+  (setq magit-save-repository-buffers t)
   :config
   (add-hook 'with-editor-mode-hook 'evil-insert-state))
 
