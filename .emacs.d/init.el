87fa0328a3912cb01f94a839ab821a5deb48d7e9
drewman <drew@drewbruce.com>
2020-09-29 21:43:27 -0700

    full window magit buffers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -197,7 +197,9 @@
 
 ;; SECTION -- DEV
 
-(use-package magit)
+(use-package magit
+  :init
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
 (use-package projectile
   :init
