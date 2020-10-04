b4a97cb4b60f9c1845deda27dba3e89efa8d8765
drewman <drew@drewbruce.com>
2020-10-04 13:59:56 -0700

    bring keybinds back into init file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(uniquify which-key use-package spell-fu rainbow-delimiters projectile moody modus-vivendi-theme exec-path-from-shell evil-magit evil-escape doom-modeline dashboard counsel company all-the-icons-ivy-rich)))
+   '(restart-emacs emacs-restart general uniquify which-key use-package spell-fu rainbow-delimiters projectile moody modus-vivendi-theme exec-path-from-shell evil-magit evil-escape doom-modeline dashboard counsel company all-the-icons-ivy-rich)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
