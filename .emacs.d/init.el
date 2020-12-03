dd183e39af033d3bd70c6f657aae1d8a5b1a9f74
Drew Bruce <drew.bruce@nike.com>
2020-12-03 12:57:20 -0800

    update org-agenda config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(restart-emacs emacs-restart general uniquify which-key use-package spell-fu rainbow-delimiters projectile moody modus-vivendi-theme exec-path-from-shell evil-magit evil-escape doom-modeline dashboard counsel company all-the-icons-ivy-rich)))
+   '(org-sidebar org restart-emacs emacs-restart general uniquify which-key use-package spell-fu rainbow-delimiters projectile moody modus-vivendi-theme exec-path-from-shell evil-magit evil-escape doom-modeline dashboard counsel company all-the-icons-ivy-rich))
+ '(safe-local-variable-values
+   '((eval progn
+           (org-babel-goto-named-src-block "startblock")
+           (org-babel-execute-src-block)
+           (outline-hide-sublevels 1))
+     (org-confirm-babel-evaluate))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
